11c6bdbc03153d92502c763ad611dc150e634da1
Engineer Youssef Mahmoud <youssefaboali122@gmail.com>
2026-07-29 21:26:42 +0300

    Delete _check_db.py

--- _check_db.py
+++
@@ -1,7 +0,0 @@
-import sys
-import pymongo
-mongo = pymongo.MongoClient("mongodb://localhost:27017/")
-db = mongo["financial_rag"]
-col = db["raw_chunks"]
-print(f"Mongo raw_chunks count: {col.count_documents({})}")
-mongo.close()
